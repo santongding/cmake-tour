7153592eef640e27f5f2c6f1e0f28c26a3831d5b
santongding <1349880437@qq.com>
2022-07-16 16:38:05 +0800

    finish step 2

--- tutorial.cxx
+++ tutorial.cxx
@@ -3,7 +3,10 @@
 #include <iostream>
 #include <string>
 #include "TutorialConfig.h"
-#include "MathFunctions.h"
+#ifdef USE_MYMATH
+#  include "MathFunctions.h"
+#endif
+
 
 int main(int argc, char* argv[])
 {
@@ -19,8 +22,11 @@ int main(int argc, char* argv[])
    const double inputValue = std::stod(argv[1]);
 
   // calculate square root
-  //const double outputValue = sqrt(inputValue);
-  const double outputValue = mysqrt(inputValue);
+#ifdef USE_MYMATH
+    const double outputValue = mysqrt(inputValue);
+#else
+    const double outputValue = sqrt(inputValue);
+#endif
 
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
