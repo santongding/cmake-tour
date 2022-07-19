887d74e01134d072378c3957943bfeac5a91b5b0
santongding <1349880437@qq.com>
2022-07-19 16:56:56 +0800

    finish step 9  Selecting Static or Shared Libraries

--- tutorial.cxx
+++ tutorial.cxx
@@ -1,11 +1,8 @@
 // A simple program that computes the square root of a number
-#include <cmath>
 #include <iostream>
 #include <string>
 #include "TutorialConfig.h"
-#ifdef USE_MYMATH
 #include "MathFunctions.h"
-#endif
 
 
 int main(int argc, char* argv[])
@@ -22,11 +19,8 @@ int main(int argc, char* argv[])
    const double inputValue = std::stod(argv[1]);
 
   // calculate square root
-#ifdef USE_MYMATH
-    const double outputValue = mysqrt(inputValue);
-#else
-    const double outputValue = sqrt(inputValue);
-#endif
+    const double outputValue = mathfunctions::sqrt(inputValue);
+
 
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
